2bde70231f4c99bdd1b6781f901b19db3b2948b5
Rohith441438 <rohithvinay889@gmail.com>
2024-07-23 18:48:21 +0000

    Required models are added and Changes in function are done.

---
+++ EshopOnWebTaskFunctionAp/Models/OrderDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EshopOnWebTaskFunctionAp.Models
+{
+    public class OrderDetails
+    {
+        public string id { get; set; }
+        public string Type { get; set; }    
+        public Address Address { get; set; }
+        public List<Item> Items { get; set; }
+        public decimal FinalPrice { get; set; }
+    }
+}
